f57913390c88db35a20e5c8c97754581f3a2ec16
grushevsky.nikolai <grushevskynikolai@gmail.com>
2023-12-13 18:20:26 +0300

    add modal, add redux

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,24 @@
-import React, {Suspense, useState} from 'react';
-import './styles/index.scss';
-import {classNames} from 'shared/lib/classNames/classNames';
-import {useTheme} from 'app/providers/ThemeProvider';
-import {AppRouter} from 'app/providers/router';
-import {Navbar} from 'widgets/Navbar';
-import {Sidebar} from 'widgets/Sidebar';
-import {Modal} from 'shared/ui/Modal/Modal';
+import React, { Suspense } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 function App() {
-  const {theme} = useTheme();
-  const [isOpen, setIsOpen] = useState(false);
+    const { theme } = useTheme();
 
-
-  return (
-    <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback="">
-        <Navbar />
-        <button onClick={() => setIsOpen(true)}>TOGGLE</button>
-        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} >
-    
-        </Modal>
-        <div className="content-page">
-          <Sidebar />
-          <AppRouter />
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  );
+    );
 }
 
 export default App;
